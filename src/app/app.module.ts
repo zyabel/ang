096d53deb93536bb8b51e349ca92257ed56904ed
zyabel <zyabel@gmail.com>
2018-09-05 10:34:33 +0300

    New features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { Header } from './components/header/header.component';
-import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Header,
-    TodoComponent
-  ],
+   ],
   imports: [
     BrowserModule,
     FormsModule
